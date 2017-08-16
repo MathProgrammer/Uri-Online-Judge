2f78b2bbd5462a56ce9aaa28bacaa6205c15a136
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2017-08-16 18:32:00 +0530

    Add files via upload

---
+++ C Programs/C Programs - 2/Prime Number.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+
+int is_Prime(int n)
+{
+    for(int i = 2; i*i <= n; i++)
+    {
+        if(n%i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    int no_of_numbers;
+    scanf("%d", &no_of_numbers);
+
+    while(no_of_numbers--)
+    {
+        int number_i;
+        scanf("%d", &number_i);
+        printf("%d ", number_i);
+        printf(is_Prime(number_i) ? "eh primo\n" : "nao eh primo\n");
+    }
+
+    return 0;
+}
